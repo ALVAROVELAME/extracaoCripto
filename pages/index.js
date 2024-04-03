623fa228fd0cc9e5ab77b7f76cad86b951fb7b6a
ALVAROVELAME <136656117+ALVAROVELAME@users.noreply.github.com>
2024-04-02 23:49:35 -0300

    atualizacao de conserto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <div>Home</div>
+    return 
+    <>
+        <h1>Home</h1>
+        <h2>meu scraping </h2>
+        <div>
+            <p>Ola, Mundo!</p>
+        </div>
+    </>
 }
 
-export default Home
+export default Home;
